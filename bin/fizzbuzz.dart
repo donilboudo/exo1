4e3d5be934c197c90d61ea0e69b0856f7605ea42
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-11 17:47:43 -0400

    add doc; format code

--- bin/fizzbuzz.dart
+++ bin/fizzbuzz.dart
@@ -1,10 +1,11 @@
  main() {
-  for(int i=0; i<=100;i++){
-    if(i%3==0){
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
       print('Fizz');
-    }
-      if(i%5==0){
-        print('Buzz');
-      }
+    } else if (i % 5 == 0){
+      print('Buzz');
+    } else {
+      print(i);
     }
   }
+}
